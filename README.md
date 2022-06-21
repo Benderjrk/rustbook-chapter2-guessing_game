8ee4325dffa13a120d451114ffb0eee438b1ca45
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-06-20 21:14:01 -0400

    Taking user input and displaying to console

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rustbook-chapter2-guessing_game
+# Rust Documentation
+
+## Chapter Two (https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html)
+
+### The Guessing Game Example Code
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!"); // Guide user to guess a number
 
-    println!("Please input your guess.");
+    println!("Please input your guess."); // Guid user to input a number
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // Create a mutable String in place of guess. Use ::new(); is UTF-8 encoded text.
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    io::stdin() // Using the standard input/output library 
+        .read_line(&mut guess) // Read line that points to a mutable guess variable. Replacing new String with input value
+        .expect("Failed to read line"); // Small error handling
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess); // Printing out the users guess
 }
