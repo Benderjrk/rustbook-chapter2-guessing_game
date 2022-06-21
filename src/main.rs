5a93ebd027e8c53cd1da7025830cb379f507133d
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-06-21 00:13:32 -0400

    Allowing secrect number and guess comparision

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
+use std::cmp::Ordering; // trait defines methods to check secret numbers
+use rand::Rng; // trait defines methods that random number generators implement
+
 
 fn main() {
     println!("Guess the number!"); // Guide user to guess a number
 
+    let secret_number = rand::thread_rng().gen_range(1..101); // getting a range of 1-100 could write as 1..=100 instead
+
+    println!("The secret number is: {}", secret_number); // printing out the secret number
     println!("Please input your guess."); // Guid user to input a number
 
     let mut guess = String::new(); // Create a mutable String in place of guess. Use ::new(); is UTF-8 encoded text.
@@ -11,5 +17,13 @@ fn main() {
         .read_line(&mut guess) // Read line that points to a mutable guess variable. Replacing new String with input value
         .expect("Failed to read line"); // Small error handling
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number"); // replacing guess with a trimmed and parsed number to compare to
+
     println!("You guessed: {}", guess); // Printing out the users guess
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
